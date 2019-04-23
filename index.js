a8e09a158cf09c309450fc7889fe5a7cd30b9208
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-04-23 22:14:38 +0300

    Routing: Parameters

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ import data from "./data/data";
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) =>
-  // get data first
-  res.json(data)
-);
-
 // this is for the public folder on path /
 app.use(express.static("public"));
 
 // this is for images folder on path images
 app.use("/images", express.static("images"));
+
+app.get("/", (req, res) =>
+  // get data first
+  res.json(data)
+);
+
+app.get("/item/:id", (req, res) => {
+  console.log(req.params.id);
+  let user = Number(req.params.id);
+  console.log(user);
+  console.log(data[user]);
+  res.send(data[user]);
+});
 
 app.post("/newItem", (req, res) =>
   res.send(`a post request with /newItem route on port ${PORT}`)
